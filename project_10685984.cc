a3e44f7de6174d867c1d03111a031fd6132abada
HafizDevs <32616060+HafizDevs@users.noreply.github.com>
2018-12-21 23:52:48 +0000

    Add files via upload

---
+++ project_10685984.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int i;
+struct details{
+string firstname[i],lastname[i],grade[i],gender[i];
+int age[i];
+};
+struct details_1{
+string id[i],firstname[i],lastname[i],gender[i];
+int age[i];
+};
+
+int main()
+{
+details student;
+details_1 staff;
+
+return 0;
+}
